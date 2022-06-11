59c4a75fef7ebbc19fdf68fe1cfe5744181c98eb
Munhoz1 <victormunhoz.dev@gmail.com>
2022-06-11 17:40:49 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/controllers/ProcutoCotroller.java
@@ -0,0 +1,37 @@
+package br.com.cotiinformatica.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.cotiinformatica.requests.ProdutoPostRequest;
+import br.com.cotiinformatica.requests.ProdutoPutRequest;
+
+@Controller
+public class ProcutoCotroller {
+
+	@RequestMapping(value = "/api/procutos", method = RequestMethod.POST)
+	public ResponseEntity<String> post(@RequestBody ProdutoPostRequest request) {
+		return ResponseEntity.status(HttpStatus.OK).body("Produto cadastrado com sucesso.");
+	}
+
+	@RequestMapping(value = "/api/produtos", method = RequestMethod.PUT)
+	public ResponseEntity<String> put(@RequestBody ProdutoPutRequest request) {
+		return ResponseEntity.status(HttpStatus.OK).body("Produto atualizado com sucesso.");
+	}
+
+	@RequestMapping(value = "/api/produtos/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<String> delete(@PathVariable("id") Integer id) {
+		return ResponseEntity.status(HttpStatus.OK).body("Produto excluído com sucesso");
+	}
+
+	@RequestMapping(value = "/api/produtos", method = RequestMethod.GET)
+	public ResponseEntity<String> get() {
+		return ResponseEntity.status(HttpStatus.OK).body(null);
+
+	}
+}
